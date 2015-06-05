2114b3f727d07116ff0784b1286d60f879ff9664
Jeremy Larkin <jeremy@imgix.com>
2015-06-05 10:03:44 -0700

    update error codes

--- include/netparse/error.h
+++ include/netparse/error.h
@@ -1,9 +1,11 @@
 #ifndef NETPARSE_ERROR_H
 #define NETPARSE_ERROR_H
 
-#define NP_ESTATE  -1 /* parser state is invalid */
-#define NP_ESYNTAX -2 /* protocol error */
-#define NP_ESIZE   -3 /* size of value exceeded maximum allowed */
+#define NP_ESYSTEM -1 /* system error */
+#define NP_ESTATE  -2 /* parser state is invalid */
+#define NP_ESYNTAX -3 /* protocol error */
+#define NP_ESIZE   -4 /* size of value exceeded maximum allowed */
+#define NP_ESTACK  -5 /* stack size exceeded */
 
 extern const char *
 np_strerror (int code);
--- lib/error.c
+++ lib/error.c
@@ -1,18 +1,25 @@
 #include "netparse/error.h"
 
+#include <errno.h>
+#include <string.h>
+
 static const char *undefined = "undefined error";
 static const char *str[] = {
 	"parser state is invalid",
 	"invalid syntax",
 	"size of value exceeded maximum allowed",
+	"stack size exceeded"
 };
 
 const char *
 np_strerror (int code)
 {
+	if (code == -1) {
+		return strerror (errno);
+	}
 	if (code > -1 || code < -(int)(sizeof (str) / sizeof (str[0]))) {
 		return undefined;
 	}
-	return str[(-1 * code) - 1];
+	return str[(-1 * code) - 2];
 }
 
