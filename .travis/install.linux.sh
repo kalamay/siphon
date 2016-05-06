3731a9a25c77d6b930eee8ecda7b12ea2d6ffc42
Jeremy Larkin <jeremy@imgix.com>
2016-05-05 18:12:53 -0700

    update travis build config and remove circleci

--- .travis/install.linux.sh
+++ .travis/install.linux.sh
@@ -6,5 +6,5 @@ sudo apt-get --assume-yes install ragel valgrind
 wget https://cmake.org/files/v${CMAKE_MINOR}/${CMAKE}.tar.gz
 tar -xzf ${CMAKE}.tar.gz
 mv ${CMAKE} cmake-build
-./cmake-build/bin/cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_TESTING=ON
+./cmake-build/bin/cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON
 ./cmake-build/bin/cmake --build build
