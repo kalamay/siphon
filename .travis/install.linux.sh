bd11943d3186feb7273bd9ecdca99ac57405989b
Jeremy Larkin <jeremy@imgix.com>
2016-05-05 17:45:13 -0700

    update travis build config

--- .travis/install.linux.sh
+++ .travis/install.linux.sh
@@ -1,8 +1,8 @@
 CMAKE_MINOR=3.5
 CMAKE=cmake-${CMAKE_MINOR}.2-Linux-x86_64
 
-sudo apt-get update
-sudo apt-get install ragel valgrind
+sudo apt-get --assume-yes update
+sudo apt-get --assume-yes install ragel valgrind
 curl -O https://cmake.org/files/v${CMAKE_MINOR}/${CMAKE}.tar.gz
 tar -xzf ${CMAKE}.tar.gz
 cp -r ${CMAKE}/bin bin
--- .travis/install.osx.sh
+++ .travis/install.osx.sh
@@ -1,4 +1,4 @@
 brew install cmake
 brew install ragel
-cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_TESTING=ON
+cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON
 cmake --build build
